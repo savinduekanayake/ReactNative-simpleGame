8730c6003f1ba6d0f0834229a837c0a00e48565e
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-10-10 22:21:51 +0530

    added local image to GameOver component

--- constants/default-styles.js
+++ constants/default-styles.js
@@ -1,4 +1,4 @@
-import {Stylesheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 // Gobaly manage styles
 export default StyleSheet.create({
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
+
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const GameOverScreen = props => {
     return(
         <View style={styles.screen}>
-            <Text>The Game is over!</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was:{props.userNumber} </Text>
+            <BodyText style={styles.resultText}>The Game is over!</BodyText>
+            <View style={styles.imageContainer}>
+                <Image 
+                    fadeDuration= {300}
+                    source={require('../assets/success.png')} 
+                    // source={uri: }
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
+            <View>
+                <BodyText style={styles.resultContainer}>
+                    Your phone needed 
+                    <Text style={styles.highlight}>{props.roundsNumber}</Text>  
+                    rounds to guess the number 
+                    <Text style={styles.highlight}>{props.userNumber}</Text>
+                </BodyText>   
+            </View>
+
+            {/* <BodyText>Number was:{props.userNumber} </BodyText> */}
             <Button 
                 title='NEW GAME' 
                 onPress= {props.onRestart}
@@ -20,6 +41,32 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: "100%",
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        // to get round. children will get crops
+        overflow: 'hidden',
+        margin: 30
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold',
+    },
+    resultContainer :{
+        marginHorizontal: 30, 
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
     }
 });
 
